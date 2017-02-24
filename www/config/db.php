0879433784ea48d5ee07a82c39e4e5abc919a141
wuzhongyang <wuzhongyang@360.cn>
2017-02-24 18:11:11 +0800

    消除个人签名痕迹

--- www/config/db.php
+++ www/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=123.57.41.158;dbname=presure',
-    'username' => 'root',
-    'password' => 'z20138502@wzy-360',
+    'dsn' => 'mysql:host=;dbname=',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
--- www/simulate/curl.php
+++ www/simulate/curl.php
@@ -1,4 +1,4 @@
 <?php
 
-$bash = 'curl -o /dev/null -s -w %{time_namelookup}::%{time_connect}::%{time_starttransfer}::%{time_total}::%{speed_download}"\n" "http://10.16.111.34:7777/simulate/index.php?type=fib"';
+$bash = 'curl -o /dev/null -s -w %{time_namelookup}::%{time_connect}::%{time_starttransfer}::%{time_total}::%{speed_download}"\n" "http://www/simulate/index.php?type=fib"';
 exec($bash);
